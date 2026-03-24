8790c27ec012c4363fa3f6c955c00ffa939861e6
Ewasion <formanban94@gmail.com>
2026-03-24 13:12:07 +0200

    Search Bar on CTRL+F - Ewasion

--- src/ToolTabs/CodeEditor/codeeditortab.cpp
+++ src/ToolTabs/CodeEditor/codeeditortab.cpp
@@ -8,6 +8,11 @@
 #include "filemanager.h"
 #include "globalwidgetsmanager.h"
 #include "utils.h"
+#include <QLineEdit>
+#include <QShortcut>
+#include <QKeySequence>
+#include <QTextCursor>
+#include <QHBoxLayout>
 
 #include "core/ToolTabFactory.h"
 
@@ -55,8 +60,56 @@ CodeEditorTab::CodeEditorTab(QWidget *parent)
     btnLayout->addWidget(anywayOpenBtn);
     overlayLayout->addLayout(btnLayout);
 
-    // - - Create and Init Stacked Layout Widget - -
+    // - - Create Search Bar Widget - -
+    m_searchWidget = new QWidget(this);
+    m_searchWidget->setObjectName("searchWidget");
 
+    m_searchWidget->setStyleSheet(
+        "QWidget#searchWidget {"
+        "   background-color: #2b2b2b;"
+        "   border-top: 1px solid #404040;"
+        "}"
+        "QLineEdit {"
+        "   background-color: #1e1e1e;"
+        "   color: #e0e0e0;"
+        "   border: 1px solid #555555;"
+        "   border-radius: 3px;"
+        "   padding: 3px 6px;"
+        "}"
+        "QLineEdit:focus { border: 1px solid #007acc; }" // Подсветка
+        "QPushButton {"
+        "   background-color: #3c3c3c;"
+        "   color: #e0e0e0;"
+        "   border: 1px solid #555555;"
+        "   border-radius: 3px;"
+        "   padding: 4px 10px;"
+        "}"
+        "QPushButton:hover { background-color: #4a4a4a; }"
+        "QPushButton:pressed { background-color: #2b2b2b; }"
+    );
+
+    
+    QHBoxLayout* searchLayout = new QHBoxLayout(m_searchWidget);
+    searchLayout->setContentsMargins(6, 6, 6, 6);
+    searchLayout->setSpacing(5);
+
+    m_searchLineEdit = new QLineEdit(this);
+    m_searchLineEdit->setPlaceholderText("Find...");
+    
+    m_findPrevBtn = new QPushButton("▲ Prev", this);
+    m_findNextBtn = new QPushButton("▼ Next", this);
+    m_closeSearchBtn = new QPushButton("✕", this);
+    
+    m_closeSearchBtn->setFixedWidth(30);
+
+    searchLayout->addWidget(m_searchLineEdit);
+    searchLayout->addWidget(m_findPrevBtn);
+    searchLayout->addWidget(m_findNextBtn);
+    searchLayout->addWidget(m_closeSearchBtn);
+    
+    m_searchWidget->hide();
+
+    // - - Create and Init Stacked Layout Widget - -
     auto stack = new QStackedLayout;
     stack->setStackingMode(QStackedLayout::StackAll);
     stack->addWidget(m_codeEditorWidget);
@@ -64,9 +117,39 @@ CodeEditorTab::CodeEditorTab(QWidget *parent)
 
     m_overlayWidget->hide();
 
-    this->setLayout(stack);
+    QVBoxLayout* mainLayout = new QVBoxLayout(this);
+    mainLayout->setContentsMargins(0, 0, 0, 0);
+    mainLayout->setSpacing(0);
+    mainLayout->addLayout(stack);
+    mainLayout->addWidget(m_searchWidget); 
 
-    // - - Connects - -
+    this->setLayout(mainLayout);
+
+    // - - Search Bar Connects & Shortcuts - -
+
+    // Ctrl+F
+    QShortcut* searchShortcut = new QShortcut(QKeySequence("Ctrl+F"), this);
+    searchShortcut->setContext(Qt::WidgetWithChildrenShortcut);
+    connect(searchShortcut, &QShortcut::activated, this, &CodeEditorTab::showSearchBar);
+
+    // Esc
+    QShortcut* escapeShortcut = new QShortcut(QKeySequence("Esc"), m_searchWidget);
+    escapeShortcut->setContext(Qt::WidgetWithChildrenShortcut);
+    connect(escapeShortcut, &QShortcut::activated, this, &CodeEditorTab::hideSearchBar);
+
+    connect(m_closeSearchBtn, &QPushButton::clicked, this, &CodeEditorTab::hideSearchBar);
+
+    connect(m_searchLineEdit, &QLineEdit::returnPressed, this, [this]() {
+        performSearch(false);
+    });
+
+    connect(m_findNextBtn, &QPushButton::clicked, this, [this]() { performSearch(false); });
+    connect(m_findPrevBtn, &QPushButton::clicked, this, [this]() { performSearch(true); });
+
+    connect(m_searchLineEdit, &QLineEdit::textChanged, this, [this]() {
+        performSearch(false);
+    });
+
 
     // Trigger: Menu Bar: View->wordWrap - setWordWrapMode
     connect(GlobalWidgetsManager::instance().get_IDEWindow_menuBar_view_wordWrap(),
@@ -162,4 +245,54 @@ void CodeEditorTab::saveTabData() {
     setModifyIndicator(false);
     emit dataEqual();
     emit refreshDataAllTabsSignal();
+}
+
+void CodeEditorTab::showSearchBar()
+{
+    if (!m_overlayWidget->isHidden()) return; 
+
+    m_searchWidget->show();
+    m_searchLineEdit->setFocus();
+    m_searchLineEdit->selectAll();
+}
+
+void CodeEditorTab::hideSearchBar()
+{
+    m_searchWidget->hide();
+    m_codeEditorWidget->setFocus(); 
+}
+
+void CodeEditorTab::performSearch(bool backward)
+{
+    QString query = m_searchLineEdit->text();
+    if (query.isEmpty()) {
+        m_searchLineEdit->setStyleSheet("");
+        return;
+    }
+
+    QTextDocument::FindFlags flags;
+    if (backward) flags |= QTextDocument::FindBackward;
+    
+    // Выполняем поиск
+    bool found = m_codeEditorWidget->find(query, flags);
+
+    if (!found) {
+        QTextCursor cursor = m_codeEditorWidget->textCursor();
+        cursor.movePosition(backward ? QTextCursor::End : QTextCursor::Start);
+        m_codeEditorWidget->setTextCursor(cursor);
+        
+        found = m_codeEditorWidget->find(query, flags);
+    }
+
+    if (!found) {
+        m_searchLineEdit->setStyleSheet(
+            "background-color: #4a2020;"
+            "color: #e0e0e0;"
+            "border: 1px solid #ff5555;"
+            "border-radius: 3px;"
+            "padding: 3px 6px;"
+        );
+    } else {
+        m_searchLineEdit->setStyleSheet("");
+    }
 }
